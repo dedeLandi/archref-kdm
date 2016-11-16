361cdc4522d4172367e6865b7c62fbb236fff71a
André <a_souzalandy@yahoo.com.br>
2016-11-16 16:46:20 -0200

    Atividade 2 - Wizard page 02 of the plug-in REFARCH-KDM
    Modificação de caminho para facilitar testes

--- br.ufscar.ARCHREF-KDM.ui/src/br/ufscar/archref_kdm/ui/wizardsPage/Page02SelectFileWithDrift.java
+++ br.ufscar.ARCHREF-KDM.ui/src/br/ufscar/archref_kdm/ui/wizardsPage/Page02SelectFileWithDrift.java
@@ -92,7 +92,7 @@ public class Page02SelectFileWithDrift extends WizardPage {
 		lFilePlannedArchitecture.setText("File containing the \r\nplanned architecture");
 
 		tPathFilePlanned = new Text(container, SWT.BORDER);
-		tPathFilePlanned.setText("C:\\Java\\workspaceMestradoJeeMars64\\archref-kdm\\br.ufscar.ARCHREF-KDM.ui\\tests\\br\\ufscar\\archref_kdm\\ui\\tests\\archPlan.xmi");
+		tPathFilePlanned.setText("C:\\Java\\workspaceMestradoMars64\\archref-kdm\\br.ufscar.ARCHREF-KDM.ui\\tests\\br\\ufscar\\archref_kdm\\ui\\tests\\archPlan.xmi");
 		tPathFilePlanned.setEditable(false);
 		tPathFilePlanned.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
@@ -125,7 +125,7 @@ public class Page02SelectFileWithDrift extends WizardPage {
 		lFileActualArchitecture.setText("File containing the \r\nactual architecture");
 
 		tPathFileActual = new Text(container, SWT.BORDER);
-		tPathFileActual.setText("C:\\Java\\workspaceMestradoJeeMars64\\archref-kdm\\br.ufscar.ARCHREF-KDM.ui\\tests\\br\\ufscar\\archref_kdm\\ui\\tests\\SystemExampleMVC-SimplesComDesvios_kdm.xmi");
+		tPathFileActual.setText("C:\\Java\\workspaceMestradoMars64\\archref-kdm\\br.ufscar.ARCHREF-KDM.ui\\tests\\br\\ufscar\\archref_kdm\\ui\\tests\\SystemExampleMVC-SimplesComDesvios_kdm.xmi");
 		tPathFileActual.setEditable(false);
 		tPathFileActual.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
